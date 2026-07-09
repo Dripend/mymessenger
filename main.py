ad8b8e3398206b96e7be2d0a45e1600ca1579cea
Dripend <161692150+Dripend@users.noreply.github.com>
2026-07-09 12:01:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -429,5 +429,5 @@ async def websocket_endpoint(ws: WebSocket, token: str = Query(...)):
 if __name__ == "__main__":
     import uvicorn
     import os
-    port = int(os.getenv("PORT", 8000))
+    port = int(os.getenv("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port, reload=True)
